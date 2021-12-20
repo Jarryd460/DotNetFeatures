24c16dcfe1c06c57bb51348ee4c3b5f3ecb75153
Jarryd Deane <Jarryd_Deane@trimble.com>
2021-12-20 20:34:06 +0200

    dd additional functionality to enums using SmartEnums nuget package

---
+++ SmartEnums/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SmartEnums;
+
+var free = Subscriptions.Free;
+var freeToo = Subscriptions.Free;
+
+Console.WriteLine(free == freeToo);
+
+var freeFromName = Subscriptions.FromName("Free");
+var freeFromValue = Subscriptions.FromValue(1);
+
+Console.WriteLine(freeFromName == freeFromValue);
+
+Console.WriteLine($"Discount was {Subscriptions.Vip.Discount}");
---
+++ SmartEnums/Subscriptions.cs
@@ -0,0 +1,46 @@
+﻿using Ardalis.SmartEnum;
+
+namespace SmartEnums;
+
+internal abstract class Subscriptions : SmartEnum<Subscriptions>
+{
+    public static readonly Subscriptions Free = new FreeTier();
+    public static readonly Subscriptions Premium = new PremiumTier();
+    public static readonly Subscriptions Vip = new VipTier();
+
+    public abstract double Discount { get; }
+
+    public Subscriptions(string name, int value) : base(name, value)
+    {
+    }
+
+    private sealed class FreeTier : Subscriptions
+    {
+        public override double Discount => .0;
+
+        public FreeTier() : base("Free", 1)
+        {
+
+        }
+    }
+
+    private sealed class PremiumTier : Subscriptions
+    {
+        public override double Discount => .25;
+
+        public PremiumTier() : base("Premium", 2)
+        {
+
+        }
+    }
+
+    private sealed class VipTier : Subscriptions
+    {
+        public override double Discount => .5;
+
+        public VipTier() : base("VIP", 3)
+        {
+
+        }
+    }
+}
